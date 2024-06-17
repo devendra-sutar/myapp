eca2e06563f1b718fa9840c6724447d5f839c07d
dev <sutards777@gmail.com>
2024-06-17 07:51:12 +0000

    my commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+// Respond with "changed not" for requests that hit our root "/"
+app.get('/', function (req, res) {
+  res.send('welcome dev');
+});
+
+// Listen on port 3000 by default or the port defined in the environment variables
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server is running on port ' + (process.env.PORT || 3000));
+});
+
+module.exports = app;
+
+
---
+++ test/test.js
@@ -0,0 +1,18 @@
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function() {
+  it('respond with 404 page not found', function(done) {
+    request(app)
+      .get('/nonexistentpage')
+      .expect(404)
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+          return done(err);
+        }
+        done();
+      });
+  });
+});
+
